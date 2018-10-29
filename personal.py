f59a07497ad600dce80ea3703cb08054f651b18f
itman20 <itsanstorage@gmail.com>
2018-10-29 13:07:16 -0800

    مکان اضافه کردن کارمند درست شد

--- personal.py
+++ personal.py
@@ -1,3 +1,10 @@
 from staff import database_utilities
 
-database_utilities.create_table(path="staff.db")
+#database_utilities.create_table(path="staff.db")
+id=input("id")
+name=input("name")
+family=input("family")
+bio=input("bio")
+
+
+database_utilities.add_staff(db_path="staff.db",staff_id=id,name=name,family=family,bio=bio)
--- staff/database_utilities.py
+++ staff/database_utilities.py
@@ -11,7 +11,7 @@ def create_table(path: str):
             sql = "DROP TABLE IF EXISTS tbl_personal" 
             cur.execute(sql)
             print("Drop tbl_personal successfully!")
-            sql = "CREATE TABLE tbl_personal ( personal_code int PRIMARY KEY NOT NULL,name varchar(250) NOT NULL,family varchar(250) NOT NULL);"
+            sql = "CREATE TABLE tbl_personal ( personal_code int PRIMARY KEY NOT NULL,name varchar(250) NOT NULL,family varchar(250) NOT NULL,bio varchar(250) NOT NULL);"
             cur.execute(sql)
             sql= "CREATE UNIQUE INDEX tbl_personal_personal_code_uindex ON tbl_personal (personal_code);"
             cur.execute(sql)
@@ -22,12 +22,37 @@ def create_table(path: str):
 
 def add_staff(db_path: str, staff_id: str, name: str, family: str, bio: str):
     # TODO etelate farrd azafe shavad
-    pass
+    try:
+        conn = bank.connect(db_path)
+        print("connect To DB successfully!")
+        with conn:
+            cur = conn.cursor()
+            print("Add_Staff_Join curser Into DB!")
+            sql = "INSERT INTO tbl_personal ( personal_code,name ,family ,bio) VALUES (?,?,?,?)"
+            staff = (staff_id,name , family,bio)
+            cur.execute(sql,staff)
+            print("Add_Staff_Created!")
+
+    except:
+        print("Error Connection")
+
 
 
 def update_staff(db_path: str, staff_id: str, name: str, family: str, bio: str):
     # TODO etelate farrd azafe shavad
-    pass
+    try:
+        conn = bank.connect(db_path)
+        print("Update_connect To DB successfully!")
+        with conn:
+            cur = conn.cursor()
+            print("Update_Staff_Join curser Into DB!")
+            sql = "Update tbl_personal ( personal_code,name ,family ,bio) VALUES (?,?,?,?)"
+            staff = (staff_id,name , family,bio)
+            cur.execute(sql,staff)
+            print("Add_Staff_Created!")
+            print(cur.lastrowid)
+    except:
+        print("Error Connection")
 
 
 def show_staff(db_path: str, staff_id: str):
